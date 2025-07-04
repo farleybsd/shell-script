89770247a1cb0a8ce14e081fc0ea6ad0e63df914
farleybsd <farley.t.i@hotmail.com>
2025-07-04 18:29:40 -0300

    Modulo 03
    Modulo 03

---
+++ Modulo02/cabecalho.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# nomeDoScript.sh - Breve descrição
+#
+# Site:       https://seusite.com.br
+# Autor:      SeuNome
+# Manutenção: Seu nome ou outro
+#
+# ------------------------------------------------------------------------ #
+# Aqui você deve utilizar uma descrição mais detalhada sobre o seu programa,
+# explicando a forma de utilizar.
+#
+# Exemplos:
+#     $ ./nomeDoScript.sh -d 1
+#       Neste exemplo o script será executado no modo debug nível 1.
+# ------------------------------------------------------------------------ #
+# Histórico:
+#
+#   v1.0 18/08/2018, Autor da mudança:
+#       - O que mudou?
+#
+#   v1.1 19/08/2018, Autor da mudança:
+#       - O que mudou?
+# ------------------------------------------------------------------------ #
+# Testado em:
+#   bash 4.4.19
+# ------------------------------------------------------------------------ #
+# Agradecimentos:
+#
+# Joãozinho - Encontrou um bug na parte de login.
+# Mariazinha - Enviou uma sugestão de adicionar o -h.
+# ------------------------------------------------------------------------ #
+
+# ========================= VARIÁVEIS ============================== #
+# ========================= TESTES ============================== #
+# --------------------------------------------------------------- #
+# ========================= Funcoes ============================== #
